3dad1396ccdcbc401441a2e65a2e3c7ba1ffec78
Christian Banse <oxisto@aybaze.com>
2015-08-14 23:22:12 +0200

    time fixes

--- shift.py
+++ shift.py
@@ -27,7 +27,12 @@ for path in glob.iglob(os.path.join(src_dir, '*.JPG')):
 	else:
 		image_name = basename.split("-")[6]
 		
-	should_be = "%s-%s" % (date.strftime("%d-%m-%Y-%H-%M-%S"), image_name)		
+	date += shift
+	metadata["Exif.Photo.DateTimeOriginal"].value = date
+	metadata["Exif.Photo.DateTimeDigitized"].value = date
+	metadata.write()		
+		
+	should_be = "%s-%s" % (date.strftime("%d-%m-%Y-%H-%M-%S"), image_name)
 
 	try:
 		parsed_date = datetime.datetime.strptime(basename[0:19], "%d-%m-%Y-%H-%M-%S")
@@ -36,13 +41,6 @@ for path in glob.iglob(os.path.join(src_dir, '*.JPG')):
 		new_path = os.path.join(src_dir, should_be)
 		shutil.move(path, new_path)
 		continue
-		
-	date += shift
-	metadata["Exif.Photo.DateTimeOriginal"].value = date
-	metadata["Exif.Photo.DateTimeDigitized"].value = date
-	metadata.write()
-	
-	should_be = "%s-%s" % (date.strftime("%d-%m-%Y-%H-%M-%S"), image_name)	
 			
 	if not parsed_date == date:
 		print "Mismatch in %s -> should be %s | %s" % (path, should_be, date - parsed_date)
